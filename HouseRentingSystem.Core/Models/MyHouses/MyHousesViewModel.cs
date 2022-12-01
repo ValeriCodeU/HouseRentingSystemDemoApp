34f4fa4f0c12008bfb325f062aa317e06e917e30
ValeriCodeU <valeri.yanev@icloud.com>
2022-12-01 23:56:36 +0200

    Added model and view for my houses functionality of the admin

---
+++ HouseRentingSystem.Core/Models/MyHouses/MyHousesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HouseRentingSystem.Core.Models.Houses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Models.MyHouses
+{
+	public class MyHousesViewModel
+	{
+		public IEnumerable<HouseServiceModel> AddedHouses { get; set; } = new List<HouseServiceModel>();
+
+		public IEnumerable<HouseServiceModel> RentedHouses { get; set; } = new List<HouseServiceModel>();
+	}
+}
