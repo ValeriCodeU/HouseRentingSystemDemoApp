44be68d94d055c84151fd22ed54b82abbea4170d
ValeriCodeU <valeri.yanev@icloud.com>
2022-12-02 23:49:12 +0200

    Created controller and action in admin area for houses functionalities

---
+++ HouseRentingSystem/Areas/Admin/Controllers/HousesController.cs
@@ -0,0 +1,34 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.MyHouses;
+using HouseRentingSystem.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Areas.Admin.Controllers
+{
+	public class HousesController : BaseController
+	{
+		private readonly IHouseService houseService;
+		private readonly IAgentService agentService;
+
+		public HousesController(
+			IHouseService _houseService, 
+			IAgentService _agentService)
+		{
+			houseService = _houseService;
+			agentService = _agentService;
+		}
+
+		public async Task<IActionResult> Mine()
+		{
+			var myHouses = new MyHousesViewModel();
+			var adminUserId = User.Id();
+			myHouses.RentedHouses = await houseService.AllHousesByUserIdAsync(adminUserId);
+
+			var adminAgent = await agentService.GetAgentIdAsync(adminUserId);
+			myHouses.AddedHouses = await houseService.AllHousesByAgentIdAsync(adminAgent);
+
+			return View(myHouses);
+;
+		}
+	}
+}
---
+++ HouseRentingSystem/Extensions/CustomAuthorizeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HouseRentingSystem.Extensions
+{
+	public class CustomAuthorizeAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			var user = context.HttpContext.User;
+
+			base.OnActionExecuting(context);
+		}
+	}
+}
